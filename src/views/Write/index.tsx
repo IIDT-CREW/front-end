2541828dc1319567820022bbd700a1ea5a21ae52
jongwon <wengers1004@naver.com>
2022-08-14 19:43:07 +0900

    🔥 [FEAT] : write page 추가
    유언 작성을 위한 페이지 및 기능 추가

---
+++ src/views/Write/index.tsx
@@ -0,0 +1,62 @@
+import { HTMLAttributes, TextareaHTMLAttributes, useState } from 'react'
+import styled from 'styled-components'
+import { fontSize, FontSizeProps } from 'styled-system'
+import axios from 'axios'
+
+const Write = () => {
+  const [title, setTitle] = useState('')
+  const [contents, setContents] = useState('')
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value)
+  }
+  const handleContents = (e) => {
+    setContents(e.target.value)
+  }
+  const handleClick = (e) => {
+    setContents((contents) => {
+      return `${contents} ${new Date().toLocaleTimeString([], { timeStyle: 'medium', hour12: false })} `
+    })
+  }
+  const handleSave = (e) => {
+    axios
+      .post('/api/write', {
+        title,
+        contents,
+      })
+      .then((res) => console.log(res))
+  }
+  return (
+    <div>
+      <Title value={title} onChange={handleTitle}></Title>
+      <Contents value={contents} onChange={handleContents}></Contents>
+      <button onClick={handleClick}>시간</button>
+      <button onClick={handleSave}>save다 뫤~</button>
+    </div>
+  )
+}
+
+const Title = ({ ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) => {
+  return <St.Textarea {...props} fontSize={'2rem'} placeholder="제목을 입력하세요." />
+}
+
+const Contents = ({ ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) => {
+  return <St.Textarea {...props} fontSize={'2rem'} placeholder="당신의 이야기를 적어보세요..." />
+}
+
+interface TextAreaProps extends FontSizeProps, HTMLAttributes<HTMLTextAreaElement> {}
+const St = {
+  Textarea: styled.textarea<TextAreaProps>`
+    outline: none;
+    border: none;
+    resize: none;
+    width: 100%;
+    font-weight: bold;
+    ::placeholder {
+      font-size: 1.5em;
+    }
+    ${fontSize}
+  `,
+}
+
+export default Write
