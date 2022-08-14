11e3f6ba360590f2551a42d6eb0c807cb284c643
jongwon <wengers1004@naver.com>
2022-08-14 19:42:09 +0900

    💎 [STYLE] : page 공통 padding 수정
    menu bar 사용을 위한 padding 변경

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -7,16 +7,16 @@ import Container from './Container'
 
 const StyledPage = styled(Container)`
   min-height: calc(100vh - 64px);
-  padding-top: 16px;
+  padding-top: 50px;
   padding-bottom: 16px;
 
   ${({ theme }) => theme.mediaQueries.sm} {
-    padding-top: 24px;
+    padding-top: 50px;
     padding-bottom: 24px;
   }
 
   ${({ theme }) => theme.mediaQueries.lg} {
-    padding-top: 32px;
+    padding-top: 50px;
     padding-bottom: 32px;
   }
 `
