48971217c12951f6f4c8dbc2f70a8c703751f724
jongwon <wengers1004@naver.com>
2022-08-20 19:45:24 +0900

    🔥 [FEAT] : 글 작성 전 유형 선택 모달 추가

--- src/views/Write/index.tsx
+++ src/views/Write/index.tsx
@@ -1,22 +1,42 @@
-import { HTMLAttributes, TextareaHTMLAttributes, useState } from 'react'
+import { HTMLAttributes, TextareaHTMLAttributes, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { fontSize, FontSizeProps } from 'styled-system'
 import axios from 'axios'
 import { getWill, insertWill } from 'api/will'
 import { ArrowLeft } from 'components/Common/Svg'
 import { useRouter } from 'next/router'
+import { useModal } from 'components/Common'
+import { Modal, ModalProps } from 'components/Common'
 
 const Write = () => {
   const [title, setTitle] = useState('')
   const [contents, setContents] = useState('')
   const router = useRouter()
+  const [modal, onDismiss] = useModal(
+    // <Modal>
+    // </Modal>,
+    <St.SelectPostTypeModal>
+      <St.ModalTitle>유서 작성 방식을 선택할 수 있어요.</St.ModalTitle>
 
+      <St.ModalDescription>
+        유서를 처음 작성하시는 분들을 위해 두 가지의 선택방식을 두었어요. 편하신 방법을 선택하여, 당신의 마지막 일기를
+        작성해주세요.
+      </St.ModalDescription>
+      <button>질문에 따라 유서를 적고 싶어요</button>
+      <button>제 마음대로 유서를 적고 싶어요</button>
+    </St.SelectPostTypeModal>,
+  )
+  useEffect(() => {
+    modal()
+  }, [])
   const handleTitle = (e) => {
     setTitle(e.target.value)
   }
+
   const handleContents = (e) => {
     setContents(e.target.value)
   }
+
   const handleClick = (e) => {
     setContents((contents) => {
       return `${contents} ${new Date().toLocaleTimeString([], { timeStyle: 'medium', hour12: false })} `
@@ -47,11 +67,13 @@ const Write = () => {
       })
       .then((res) => router.push('main'))
   }
+
   const handleGoToHistory = () => {
     router.push('main')
   }
+
   return (
-    <div>
+    <article>
       <St.MenuBar>
         <St.GoToHistoryButton onClick={handleGoToHistory}>
           <ArrowLeft />내 기록
@@ -63,7 +85,7 @@ const Write = () => {
       </St.MenuBar>
       {/* <Title value={title} onChange={handleTitle}></Title> */}
       <Contents value={contents} onChange={handleContents}></Contents>
-    </div>
+    </article>
   )
 }
 const Title = ({ ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) => {
@@ -76,6 +98,30 @@ const Contents = ({ ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) => 
 
 interface TextAreaProps extends FontSizeProps, HTMLAttributes<HTMLTextAreaElement> {}
 const St = {
+  ModalTitle: styled.div`
+    font-weight: 600;
+    font-size: 18px;
+    margin-top: 32px;
+  `,
+  ModalDescription: styled.div`
+    width: 410px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    margin-top: 8px;
+    text-align: center;
+  `,
+  SelectPostTypeModal: styled.div`
+    width: 586px;
+    height: 375px;
+    background: #ffffff;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.06), 0px 0px 1px rgba(0, 0, 0, 0.08);
+    border-radius: 4px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+
   MenuBar: styled.div`
     border: none;
     height: 78px;
