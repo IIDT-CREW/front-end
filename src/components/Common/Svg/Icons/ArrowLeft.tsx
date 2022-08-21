3c2290fbcdea64173000c0df3ff2b57ae8b84fe4
jongwon <wengers1004@naver.com>
2022-08-21 23:57:49 +0900

    💎 [STYLE] : ArrowLeft svg 수정

--- src/components/Common/Svg/Icons/ArrowLeft.tsx
+++ src/components/Common/Svg/Icons/ArrowLeft.tsx
@@ -4,11 +4,9 @@ import { SvgProps } from '../types'
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 24 24" {...props}>
-      <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none">
-        <path d="M8.375 14.75L1.625 8L8.375 1.25" stroke="black" />
-      </svg>
-    </Svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="16" viewBox="0 0 10 16" fill="none">
+      <path d="M8.375 14.75L1.625 8L8.375 1.25" stroke="black" />
+    </svg>
   )
 }
 export default Icon
