6a26f57eb4bb4744906c547e58716523ab865933
jongwon <wengers1004@naver.com>
2022-08-21 21:33:56 +0900

    💎 [STYLE] : modal style 적용

--- src/views/Write/index.tsx
+++ src/views/Write/index.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes, TextareaHTMLAttributes, useEffect, useState } from 'react'
+import { HTMLAttributes, TextareaHTMLAttributes, useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { fontSize, FontSizeProps } from 'styled-system'
 import axios from 'axios'
@@ -6,15 +6,24 @@ import { getWill, insertWill } from 'api/will'
 import { ArrowLeft } from 'components/Common/Svg'
 import { useRouter } from 'next/router'
 import { useModal } from 'components/Common'
-import { Modal, ModalProps } from 'components/Common'
 
-const Write = () => {
-  const [title, setTitle] = useState('')
-  const [contents, setContents] = useState('')
-  const router = useRouter()
-  const [modal, onDismiss] = useModal(
-    // <Modal>
-    // </Modal>,
+const questionList = [
+  '1. 살아오면서 가장 기뻤던 일은?',
+  '2. 살아오면서 가장 자부심을 느꼈던 일은?',
+  '3. 살아오면서 가장 노여웠던 일은?',
+  '4. 살아오면서 가장 자존심이 상했던 일은?',
+  '5. 살아오면서 가장 슬펐던 일은?',
+  '6. 살아오면서 가장 상처받았던 일은?',
+  '7. 살아오면서 가장 수치스러웠던 일은?',
+  '8. 살아오면서 가장 후회스러운 일은?',
+  '9. 살아오면서 가장 미워했던 사람은?',
+  '10. 살아오면서 가장 사랑했던 사람은?',
+  '11. 죽음을 앞두고 가장 하고 싶은 말은?',
+]
+const SelectPostTypeModal = (props) => {
+  const { handleClick } = props
+  console.log(handleClick)
+  return (
     <St.SelectPostTypeModal>
       <St.ModalTitle>유서 작성 방식을 선택할 수 있어요.</St.ModalTitle>
 
@@ -22,13 +31,25 @@ const Write = () => {
         유서를 처음 작성하시는 분들을 위해 두 가지의 선택방식을 두었어요. 편하신 방법을 선택하여, 당신의 마지막 일기를
         작성해주세요.
       </St.ModalDescription>
-      <button>질문에 따라 유서를 적고 싶어요</button>
-      <button>제 마음대로 유서를 적고 싶어요</button>
-    </St.SelectPostTypeModal>,
+      <St.ModalButton onClick={handleClick}>질문에 따라 유서를 적고 싶어요</St.ModalButton>
+      <St.ModalButton variant="primary">제 마음대로 유서를 적고 싶어요</St.ModalButton>
+    </St.SelectPostTypeModal>
   )
+}
+const Write = () => {
+  const [title, setTitle] = useState('')
+  const [contents, setContents] = useState('')
+  const router = useRouter()
+  const [isDefaultPostType, setPostType] = useState(true)
+  const handlePostType = (e) => {
+    console.log('isFalse')
+    setPostType(false)
+  }
+  const [modal] = useModal(<SelectPostTypeModal handleClick={handlePostType} />)
   useEffect(() => {
     modal()
-  }, [])
+  }, [isDefaultPostType])
+
   const handleTitle = (e) => {
     setTitle(e.target.value)
   }
@@ -71,7 +92,6 @@ const Write = () => {
   const handleGoToHistory = () => {
     router.push('main')
   }
-
   return (
     <article>
       <St.MenuBar>
@@ -84,7 +104,16 @@ const Write = () => {
         </St.SaveButton>
       </St.MenuBar>
       {/* <Title value={title} onChange={handleTitle}></Title> */}
-      <Contents value={contents} onChange={handleContents}></Contents>
+      {isDefaultPostType ? (
+        <Contents value={contents} onChange={handleContents}></Contents>
+      ) : (
+        questionList.map((question) => (
+          <>
+            <div>{question}</div>
+            <Contents value={contents} onChange={handleContents}></Contents>
+          </>
+        ))
+      )}
     </article>
   )
 }
@@ -97,6 +126,7 @@ const Contents = ({ ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) => 
 }
 
 interface TextAreaProps extends FontSizeProps, HTMLAttributes<HTMLTextAreaElement> {}
+type variant = 'primary' | 'secondary'
 const St = {
   ModalTitle: styled.div`
     font-weight: 600;
@@ -121,7 +151,31 @@ const St = {
     flex-direction: column;
     align-items: center;
   `,
+  ModalButton: styled.button<{ variant?: variant }>`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 14px 16px;
+    gap: 10px;
 
+    width: 335px;
+    height: 50px;
+    margin-top: 40px;
+    /* Color/Grayscale 2 */
+    background-color: ${({ variant, theme }) =>
+      variant === 'primary' ? theme.colors.grayscale7 : theme.colors.grayscale2};
+    color: ${({ variant, theme }) => (variant === 'primary' ? theme.colors.grayscale0 : theme.colors.grayscale7)};
+
+    border-radius: 4px;
+    border: none;
+
+    font-family: 'SUIT';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+  `,
   MenuBar: styled.div`
     border: none;
     height: 78px;
