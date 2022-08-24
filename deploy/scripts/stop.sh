138bdf9df275ba52341c7e4e23a24935623d64de
westkite1201 <westkite1201@gmail.com>
2022-08-24 22:48:16 +0900

    🔨 [FIX] : stop.sh script 오류 수정

--- deploy/scripts/stop.sh
+++ deploy/scripts/stop.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "bye"
+
+SERVICE_PID=`ps -ef | grep -ie '-p 6060'  | grep -v grep | awk '{print $2}'`
+APPLICATION_NAME='ifs'
+
+echo "SERVICE_PID = ${SERVICE_PID}"
+if [[ "" !=   "$SERVICE_PID" ]]; then
+        echo
+        echo "==[${APPLICATION_NAME}](${SERVICE_PID}) stoping..."
+        kill -15 $SERVICE_PID
+        echo "==[${APPLICATION_NAME}](${SERVICE_PID}) stopped..."
+        echo
+else
+        echo
+        echo "==[${APPLICATION_NAME}] service is NOT running..."
+        echo
+fi
