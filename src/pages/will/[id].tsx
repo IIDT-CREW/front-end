2fcd311aba9acafa63e48f389cbf198ec59132ff
westkite1201 <westkite1201@gmail.com>
2022-08-26 11:34:16 +0900

    🔥 [FEAT] : will page 서버사이드 props 추가

--- src/pages/will/[id].tsx
+++ src/pages/will/[id].tsx
@@ -1,7 +1,22 @@
+import type { GetServerSideProps, NextPage } from 'next'
 import Will from 'views/Will'
-
+import { dehydrate, QueryClient, useQuery } from 'react-query'
+import { getWill } from 'api/will'
 const WillPage = () => {
   return <Will />
 }
 
 export default WillPage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const queryClient = new QueryClient()
+  const { id } = context.query
+
+  await queryClient.prefetchQuery('getWill', () => getWill(id as string))
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
+}
