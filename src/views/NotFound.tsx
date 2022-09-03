7d713820ee81157c70f8e9a038d637d3b4a6d19f
westkite1201 <westkite1201@gmail.com>
2022-09-03 14:30:11 +0900

    🔥 [FEAT] : 404 Page 추가

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,30 +1,62 @@
 import styled from 'styled-components'
-import { Button, Heading, Text, LogoIcon } from 'components/Common'
+import { Button, Heading, Text, LogoIcon, Box } from 'components/Common'
 import Page from 'components/Layout/Page'
 import Link from 'next/link'
+import { MainButton } from 'views/Home'
 
 const StyledNotFound = styled.div`
-  align-items: center;
   display: flex;
-  flex-direction: column;
-  height: calc(100vh - 64px);
+  align-items: center;
   justify-content: center;
+  flex-direction: column;
 `
 
 const NotFound = () => {
   return (
-    <Page>
+    <Box style={{ minHeight: 'calc(100vh - 64px)', justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
+      {/* <img
+        src="/images/home/joshua-sortino-XMcoTHgNcQA-unsplash.jpg"
+        alt=""
+        style={{
+          width: '100%',
+          minHeight: 'calc(100vh - 64px)',
+          position: 'absolute',
+          zIndex: '-1',
+          background:
+            'linear-gradient(0deg, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url("/images/home/joshua-sortino-XMcoTHgNcQA-unsplash.jpg)',
+        }}
+      /> */}
+      <Box
+        style={{
+          width: '100%',
+          minHeight: 'calc(100vh - 64px)',
+          position: 'absolute',
+          zIndex: '-1',
+          background:
+            'linear-gradient(0deg, rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)), url(/images/home/joshua-sortino-XMcoTHgNcQA-unsplash.jpg)',
+        }}
+      ></Box>
       <StyledNotFound>
-        <LogoIcon width="64px" mb="8px" />
-        <Heading scale="xxl">404</Heading>
-        <Text mb="16px">{'Oops, page not found.'}</Text>
-        <Link href="/" passHref>
-          <Button as="button" scale="sm">
-            {'Back Home'}
-          </Button>
+        <Text fontSize="36px" mb="18px">
+          길을 잃으셨나요?
+        </Text>
+        <Text style={{ fontFamily: 'SUIT' }}> 괜찮아요. 저희가 안내할게요.</Text>
+        <Text style={{ fontFamily: 'SUIT' }}> 찾으시는 페이지의 주소가 잘못 입력되었거나,</Text>
+        <Text style={{ fontFamily: 'SUIT' }} mb="18px">
+          주소의 변경 혹은 삭제로 인해 사용하실수 없습니다.
+        </Text>
+
+        <Text style={{ fontFamily: 'SUIT' }}>
+          입력하신 페이지의 주소가 정확한지 다시 한 번 획인해주시고, 같은 문제가 또 발생한다면
+        </Text>
+        <Text style={{ fontFamily: 'SUIT' }} mb="18px">
+          ifteam@gmail.com으로 알려주세요.
+        </Text>
+        <Link href="/">
+          <MainButton style={{ fontFamily: 'SUIT' }}>홈으로 가기</MainButton>
         </Link>
       </StyledNotFound>
-    </Page>
+    </Box>
   )
 }
 
