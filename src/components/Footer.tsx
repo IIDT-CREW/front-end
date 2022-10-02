e22d8c102dd0277a669d6a6fd31af49ff75dc1ec
westkite1201 <westkite1201@gmail.com>
2022-10-02 22:17:38 +0900

    🔥 [FEAT] : footer notion link 추가, write시 보이지 않게 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,23 @@ import { Box, Text, Flex } from 'components/Common'
 import { FOOTER_HEIGHT } from 'config/constants/default'
 
 const Footer = () => {
+  const handleNotion = () => {
+    window.open('https://iidtcrew.notion.site/IIDT-CREW-74a141ff2a93486594249a8c84fe9270', '_blank')
+  }
+
   return (
     <Box height={`${FOOTER_HEIGHT}px`} padding="20px" borderTop="1px solid #D4D4D4 " borderBottom="none">
-      <Text fontSize="12px">개인정보수집방침 이용약관</Text>
+      {/* <Text fontSize="12px">개인정보수집방침 이용약관</Text> */}
+      <Text fontSize="12px" mr="10px">
+        IIDT
+      </Text>
       <Flex>
-        <Text fontSize="12px">팀 프로젝트 명 | IIDT</Text>
-        <Text fontSize="12px">팀 소개 Notion</Text>
-        <Text fontSize="12px">Email | ifteam@gmail.com</Text>
+        <Text mr="10px" fontSize="12px" onClick={handleNotion} style={{ cursor: 'pointer' }}>
+          * 팀 소개 | Notion
+        </Text>
+        <Text mr="10px" fontSize="12px">
+          * Email | iidtcrew@gmail.com
+        </Text>
       </Flex>
     </Box>
   )
