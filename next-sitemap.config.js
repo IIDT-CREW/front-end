77680349176fbdecec15ededad9e3ab6eaa63afa
westkite1201 <westkite1201@gmail.com>
2022-12-14 00:09:35 +0900

    🔥 [FEAT] : next-sitemap 설정

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://if-i-die-tomorrow.com',
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+  // ...other options
+}
