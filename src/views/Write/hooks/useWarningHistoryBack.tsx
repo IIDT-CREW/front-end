38d47e28c5662b3bfa7640d40ae035074a72078b
westkite1201 <westkite1201@gmail.com>
2023-01-06 18:54:03 +0900

    🔨 [FIX] : ios 글 작성시 오류 나는 이슈 수정 -safari 브라우저에선 push state 100 times 이상 발생시 오류

--- src/views/Write/hooks/useWarningHistoryBack.tsx
+++ src/views/Write/hooks/useWarningHistoryBack.tsx
@@ -1,12 +1,12 @@
 import { useModal } from 'components/Common'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 import WarningHistoryBackModal from '../components/modal/WarningHistoryBackModal'
 
 const useWarningHistoryBack = ({ title, contents, goToBack, page }) => {
   const isWriteDown = () => !contents.every((value) => value.length === 0)
   const [presentWarningHistoryBackModal] = useModal(<WarningHistoryBackModal goToBack={goToBack} />)
   const isWriteDownTitleAndContent = title !== '' || contents[page] !== '' || isWriteDown()
-
+  const isInsertPushState = useRef(false)
   const preventGoBack = useCallback(() => {
     if (isWriteDownTitleAndContent) {
       presentWarningHistoryBackModal()
@@ -17,8 +17,9 @@ const useWarningHistoryBack = ({ title, contents, goToBack, page }) => {
 
   useEffect(() => {
     if (!isWriteDownTitleAndContent) return
-    if (isWriteDownTitleAndContent) {
+    if (isWriteDownTitleAndContent && !isInsertPushState.current) {
       history.pushState(null, '', location.href)
+      isInsertPushState.current = true
     }
     window.addEventListener('popstate', preventGoBack)
     return () => {
